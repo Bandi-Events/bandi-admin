ab5e973031500a3c0634f6c4491a3611a5fbcaed
Keith Brewster <keith.brewster22@gmail.com>
2021-10-15 15:59:50 -0400

    feat(pages): add dashboard page

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,80 @@
+import CardLineChart from 'components/Cards/CardLineChart';
+import CardBarChart from 'components/Cards/CardBarChart';
+import CardPageVisits from 'components/Cards/CardPageVisits';
+import CardSocialTraffic from 'components/Cards/CardSocialTraffic';
+import CardStats from 'components/Cards/CardStats';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <div className="flex flex-wrap pt-8">
+        <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+          <CardStats
+            statSubtitle="User Events"
+            statTitle="350,897"
+            statArrow="up"
+            statPercent="3.48"
+            statPercentColor="text-emerald-500"
+            statDescription="Since last month"
+            statIconName="far fa-chart-bar"
+            statIconColor="bg-red-500"
+          />
+        </div>
+        <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+          <CardStats
+            statSubtitle="New Users"
+            statTitle="2,356"
+            statArrow="down"
+            statPercent="3.48"
+            statPercentColor="text-red-500"
+            statDescription="Since last week"
+            statIconName="fas fa-chart-pie"
+            statIconColor="bg-orange-500"
+          />
+        </div>
+        <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+          <CardStats
+            statSubtitle="Transactions"
+            statTitle="924"
+            statArrow="down"
+            statPercent="1.10"
+            statPercentColor="text-orange-500"
+            statDescription="Since yesterday"
+            statIconName="fas fa-users"
+            statIconColor="bg-pink-500"
+          />
+        </div>
+        <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+          <CardStats
+            statSubtitle="Performance"
+            statTitle="49,65%"
+            statArrow="up"
+            statPercent="12"
+            statPercentColor="text-emerald-500"
+            statDescription="Since last month"
+            statIconName="fas fa-percent"
+            statIconColor="bg-lightBlue-500"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap mt-8">
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <CardLineChart />
+        </div>
+        <div className="w-full xl:w-4/12 px-4">
+          <CardBarChart />
+        </div>
+      </div>
+      <div className="flex flex-wrap mt-4">
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <CardPageVisits />
+        </div>
+        <div className="w-full xl:w-4/12 px-4">
+          <CardSocialTraffic />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
