0f623f4b8a149b26eb921594247cf567a61c2102
Keith Brewster <keith.brewster22@gmail.com>
2021-10-15 16:01:56 -0400

    refactor(app): set up app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-function App() {
+import Sidebar from 'components/Sidebar';
+import Footer from 'components/Footer';
+import Dashboard from 'pages/Dashboard';
+import Users from 'pages/Users';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      <div className="relative md:ml-64 bg-gray-50">
+        <main className="px-4 md:px-10 mx-auto w-full">
+          <Switch>
+            <Route path="/dashboard" exact component={Dashboard} />
+            <Route path="/users" exact component={Users} />
+            <Redirect from="/" to="/dashboard" />
+          </Switch>
+          <Footer />
+        </main>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
